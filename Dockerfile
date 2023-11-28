ff5d86890381e1e74877e7df9c6fc1b098a74e3c
Yuta Katsuma <ktsm@mba.local>
2023-11-28 15:28:30 +0900

    docker compose v3

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ruby:3.2.2
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
     build-essential \
-    nodejs \
+    vim \
 #無駄なインストールを省き、aptインストール時のキャッシュを削除してくれる。
     && apt-get clean \
     && rm -rf /var/lib/apt/list/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
+# Rails s でエラーを引き起こす server.pid を削除するためのシェル
+
 #!/bin/bash
-set -e
-# Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
+set -e     # set -e は「エラーが発生するとスクリプトを終了する」オプション
+
 rm -f /myapp/tmp/pids/server.pid
-# コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
-exec "$@"
+
+exec "$@"    # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの）
